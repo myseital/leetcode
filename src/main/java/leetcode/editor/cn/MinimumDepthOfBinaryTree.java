ab588afedca118bfe244ce3a8a2d2b50c6a8e98a
myseital <myseital24@gmail.com>
2022-02-24 18:06:07 +0800

    二叉树的最小深度

--- src/main/java/leetcode/editor/cn/MinimumDepthOfBinaryTree.java
+++ src/main/java/leetcode/editor/cn/MinimumDepthOfBinaryTree.java
@@ -55,60 +55,60 @@ public class MinimumDepthOfBinaryTree {
      * }
      */
     class Solution {
-//        public int minDepth(TreeNode root) {
-//            if (root == null) {
-//                return 0;
-//            }
-//
-//            if (root.left == null && root.right == null) {
-//                return 1;
-//            }
-//
-//            int min_depth = Integer.MAX_VALUE;
-//            if (root.left != null) {
-//                min_depth = Math.min(minDepth(root.left), min_depth);
-//            }
-//            if (root.right != null) {
-//                min_depth = Math.min(minDepth(root.right), min_depth);
-//            }
-//
-//            return min_depth + 1;
-//        }
-
-        class QueueNode {
-            TreeNode node;
-            int depth;
-
-            public QueueNode(TreeNode node, int depth) {
-                this.node = node;
-                this.depth = depth;
-            }
-        }
-
         public int minDepth(TreeNode root) {
             if (root == null) {
                 return 0;
             }
 
-            Queue<QueueNode> queue = new LinkedList<QueueNode>();
-            queue.offer(new QueueNode(root, 1));
-            while (!queue.isEmpty()) {
-                QueueNode nodeDepth = queue.poll();
-                TreeNode node = nodeDepth.node;
-                int depth = nodeDepth.depth;
-                if (node.left == null && node.right == null) {
-                    return depth;
-                }
-                if (node.left != null) {
-                    queue.offer(new QueueNode(node.left, depth + 1));
-                }
-                if (node.right != null) {
-                    queue.offer(new QueueNode(node.right, depth + 1));
-                }
+            if (root.left == null && root.right == null) {
+                return 1;
             }
 
-            return 0;
+            int minDepth = Integer.MAX_VALUE;
+            if (root.left != null) {
+                minDepth = Math.min(minDepth(root.left), minDepth);
+            }
+            if (root.right != null) {
+                minDepth = Math.min(minDepth(root.right), minDepth);
+            }
+
+            return minDepth + 1;
         }
+
+//        class QueueNode {
+//            TreeNode node;
+//            int depth;
+//
+//            public QueueNode(TreeNode node, int depth) {
+//                this.node = node;
+//                this.depth = depth;
+//            }
+//        }
+//
+//        public int minDepth(TreeNode root) {
+//            if (root == null) {
+//                return 0;
+//            }
+//
+//            Queue<QueueNode> queue = new LinkedList<QueueNode>();
+//            queue.offer(new QueueNode(root, 1));
+//            while (!queue.isEmpty()) {
+//                QueueNode nodeDepth = queue.poll();
+//                TreeNode node = nodeDepth.node;
+//                int depth = nodeDepth.depth;
+//                if (node.left == null && node.right == null) {
+//                    return depth;
+//                }
+//                if (node.left != null) {
+//                    queue.offer(new QueueNode(node.left, depth + 1));
+//                }
+//                if (node.right != null) {
+//                    queue.offer(new QueueNode(node.right, depth + 1));
+//                }
+//            }
+//
+//            return 0;
+//        }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
