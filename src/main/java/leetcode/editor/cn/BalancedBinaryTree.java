5ff10e1474f4cf6fefb3bb0bd653436544f70a31
myseital <myseital24@gmail.com>
2022-03-14 15:04:05 +0800

    两个列表的最小索引总和

---
+++ src/main/java/leetcode/editor/cn/BalancedBinaryTree.java
@@ -0,0 +1,102 @@
+package leetcode.editor.cn;
+
+//给定一个二叉树，判断它是否是高度平衡的二叉树。 
+//
+// 本题中，一棵高度平衡二叉树定义为： 
+//
+// 
+// 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,2,2,3,3,null,null,4,4]
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：true
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中的节点数在范围 [0, 5000] 内 
+// -10⁴ <= Node.val <= 10⁴ 
+// 
+// Related Topics 树 深度优先搜索 二叉树 👍 910 👎 0
+
+public class BalancedBinaryTree {
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        // 自底向上的递归
+        public boolean isBalanced(TreeNode root) {
+            return height(root) >= 0;
+        }
+
+        public int height(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            int leftHeight = height(root.left);
+            int rightHeight = height(root.right);
+            if (leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1) {
+                return -1;
+            } else {
+                return Math.max(leftHeight, rightHeight) + 1;
+            }
+        }
+
+        // 自顶向下的递归
+//        public boolean isBalanced(TreeNode root) {
+//            if (root == null) {
+//                return true;
+//            } else {
+//                return Math.abs(height(root.left) - height(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+//            }
+//        }
+//
+//        public int height(TreeNode root) {
+//            if (root == null) {
+//                return 0;
+//            } else {
+//                return Math.max(height(root.left), height(root.right)) + 1;
+//            }
+//        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
