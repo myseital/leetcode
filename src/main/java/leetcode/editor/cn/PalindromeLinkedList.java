eeac9679f8e0ac266889c9d9d8ec22a4c03bc6e6
myseital <myseital24@gmail.com>
2022-04-02 22:09:34 +0800

    回文链表

---
+++ src/main/java/leetcode/editor/cn/PalindromeLinkedList.java
@@ -0,0 +1,77 @@
+package leetcode.editor.cn;
+
+//给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,2,1]
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,2]
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目在范围[1, 10⁵] 内 
+// 0 <= Node.val <= 9 
+// 
+//
+// 
+//
+// 进阶：你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？ 
+// Related Topics 栈 递归 链表 双指针 👍 1329 👎 0
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromeLinkedList {
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> vals = new ArrayList<Integer>();
+
+        // 将链表的值复制到数组中
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        // 使用双指针判断是否回文
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ src/main/java/leetcode/editor/cn/ReverseLinkedList.java
@@ -0,0 +1,87 @@
+package leetcode.editor.cn;
+
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+// 
+// 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,2]
+//输出：[2,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围是 [0, 5000] 
+// -5000 <= Node.val <= 5000 
+// 
+//
+// 
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？ 
+// 
+// 
+// Related Topics 递归 链表 👍 2407 👎 0
+
+public class ReverseLinkedList {
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+//            //申请节点，pre和 cur，pre指向null
+//            ListNode pre = null;
+//            ListNode cur = head;
+//            ListNode tmp = null;
+//            while (cur != null) {
+//                //记录当前节点的下一个节点
+//                tmp = cur.next;
+//                //然后将当前节点指向pre
+//                cur.next = pre;
+//                //pre和cur节点都前进一位
+//                pre = cur;
+//                cur = tmp;
+//            }
+//            return pre;
+
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode newHead = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return newHead;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
