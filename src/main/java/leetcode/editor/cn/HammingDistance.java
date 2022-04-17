5628ea6d37c73f4dd2019c283d801d20eeaad94a
myseital <myseital24@gmail.com>
2022-04-17 16:10:38 +0800

    汉明距离

--- src/main/java/leetcode/editor/cn/HammingDistance.java
+++ src/main/java/leetcode/editor/cn/HammingDistance.java
@@ -39,7 +39,13 @@ public class HammingDistance {
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int hammingDistance(int x, int y) {
-
+        int s = x ^ y, ret = 0;
+        while (s != 0) {
+            s &= s - 1;
+            ret++;
+        }
+        return ret;
+//        return Integer.bitCount(x ^ y);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
