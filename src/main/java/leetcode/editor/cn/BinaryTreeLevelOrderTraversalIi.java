51b1dd0945520c14eda3cf80e071b55625655179
myseital <myseital24@gmail.com>
2022-05-05 22:32:57 +0800

    练习

---
+++ src/main/java/leetcode/editor/cn/BinaryTreeLevelOrderTraversalIi.java
@@ -0,0 +1,90 @@
+package leetcode.editor.cn;
+
+//给你二叉树的根节点 root ，返回其节点值 自底向上的层序遍历 。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历） 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[15,7],[9,20],[3]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：[[1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 2000] 内 
+// -1000 <= Node.val <= 1000 
+// 
+// Related Topics 树 广度优先搜索 二叉树 👍 564 👎 0
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class BinaryTreeLevelOrderTraversalIi {
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> data = new ArrayList<>();
+        if (root == null) {
+            return data;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int currentLevelSize = queue.size();
+            for (int i = 1; i <= currentLevelSize; ++i) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            data.add(0, level);
+        }
+
+        return data;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
